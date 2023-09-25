c4290aaade1352ff88f3f5e50ea91ca428125d33
stake12 <73495992+stake12@users.noreply.github.com>
2023-09-25 09:01:59 +0000

    variables

---
+++ 01.basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "stakealert@gmail.com"
+var accountPassword = "123456"
+accountCity = "Pune"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
